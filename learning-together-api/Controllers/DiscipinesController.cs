99060e8746e99213d65e8798e9234b34e3c35ae8
chrispydizzle <chris@cpsharp.net>
2019-04-16 12:03:21 -0400

    add categories fetch to disciplines controller

--- learning-together-api/Controllers/DiscipinesController.cs
+++ learning-together-api/Controllers/DiscipinesController.cs
@@ -1,10 +1,26 @@
 namespace learning_together_api.Controllers
 {
+    using System.Collections.Generic;
     using Data;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
     using Services;
 
     public class DisciplinesController : GetDataController<Discipline>
     {
-        public DisciplinesController(IDisciplineService service, ICategoryService categoryService) : base(service) { }
+        private readonly ICategoryService categoryService;
+
+        public DisciplinesController(IDisciplineService service, ICategoryService categoryService) : base(service)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet("categories")]
+        [AllowAnonymous]
+        public IActionResult Categories()
+        {
+            IEnumerable<Category> locations = this.categoryService.GetAll();
+            return this.Ok(locations);
+        }
     }
 }
