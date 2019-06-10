58fde0fa2a82a9cd249e06477277d0ddae8a2b71
chrispydizzle <chris@cpsharp.net>
2019-06-10 14:08:29 -0400

    add attendee username to workshop query

--- learning-together-api/Data/Mappers/WorkshopAttendeeDto.cs
+++ learning-together-api/Data/Mappers/WorkshopAttendeeDto.cs
@@ -4,6 +4,8 @@ namespace learning_together_api.Data.Mappers
     {
         public int UserId { get; set; }
 
+        public string Username { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
