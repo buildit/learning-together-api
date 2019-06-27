cd6af8b7fa55028079e03b8c2a431e9e9bded07b
chrispydizzle <chris@cpsharp.net>
2019-06-27 12:49:25 +0100

    Inject exception entity into data context

--- learning-together-api/Migrations/20190627114014_exceptions.Designer.cs
+++ learning-together-api/Migrations/20190627114014_exceptions.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using learning_together_api.Data;
@@ -9,9 +10,10 @@ using learning_together_api.Data;
 namespace learningtogetherapi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190627114014_exceptions")]
+    partial class exceptions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ learning-together-api/Migrations/20190627114014_exceptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace learningtogetherapi.Migrations
+{
+    public partial class exceptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "exceptions",
+                schema: "admin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Message = table.Column<string>(nullable: true),
+                    Time = table.Column<DateTime>(nullable: false),
+                    User = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_exceptions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "exceptions",
+                schema: "admin");
+        }
+    }
+}
